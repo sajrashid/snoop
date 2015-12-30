62337e0d43d8a02d5cb675368c66872eafe86f29
Sajid Rashid <sajid.rashid@live.co.uk>
2015-12-30 23:14:01 +0000

    cleaned up again

---
+++ src/app/require.config.js
@@ -0,0 +1,12 @@
+// require.js looks for the following global when initializing
+
+
+var require = {
+    baseUrl: ".",
+    paths: {
+        "testModule": "bower_modules/modules/testModule"
+    },
+    shim: {
+      
+    }
+};
---
+++ src/app/startup.js
@@ -0,0 +1,5 @@
+require(['testModule'], function (t) {
+    var x = t.someMethod(3);
+    console.log(x);
+
+});
